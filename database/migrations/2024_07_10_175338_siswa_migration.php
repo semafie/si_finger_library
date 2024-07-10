205ad191802076d17e687302570ddc9731bc3db5
semafie <restuimamsafii04@gmail.com>
2024-07-11 01:18:42 +0700

    pertama

---
+++ database/migrations/2024_07_10_175338_siswa_migration.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nisn');
+            $table->String('student_name');
+            $table->string('class');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
---
+++ database/migrations/2024_07_10_175424_guru_migration.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nip');
+            $table->String('teacher_name');
+            $table->string('class');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher');
+    }
+};
